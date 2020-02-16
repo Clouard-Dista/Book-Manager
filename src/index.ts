63ac851e63579f17f8dcdbb0fe6b0c476f882d62
Talloir Vincent <vincenttall@hotmail.fr>
2020-02-16 11:13:33 +0100

    add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import "./lib/env";
 require('dotenv').config()
-const express = require( "express" );
+
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./config/swagger.json');
+
 const app = express();
 const port = process.env.PORT; 
+
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 // define a route handler for the default home page
 app.get( "/", ( req:any, res:any ) => {
